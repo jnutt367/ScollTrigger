faa17798e2eceea293b099bcd11aff9394ee846d
Jason Nutt <jnutt367@gmail.com>
2021-04-12 17:09:53 -0400

    commit for new animations and begin eases with Carl

--- D3.js
+++ D3.js
@@ -4,8 +4,11 @@ d3.selectAll("h2").style("color", function() {
     return "hsl(" + Math.random() * 360 + ",100%,50%)";
 });
 
-d3.select("body").style("background-color", "white");
+d3.select("body").style("background-color", "beige");
 
 d3.selectAll("h1").style("color", function() {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+});
+d3.selectAll("h1").style("background-color", function() {
     return "hsl(" + Math.random() * 360 + ",100%,50%)";
 });
--- gsap.js
+++ gsap.js
@@ -10,3 +10,4 @@ gsap.fromTo(".circle", { x: 50, y: -100 }, {
 
 gsap.fromTo(".rect", { x: 100 }, { duration: "2.5", y: "-150", rotate: "35" });
 gsap.to(".h2", { backgroundColor: "black", padding: "20" });
+gsap.to("h1", { padding: "20", borderRadius: "15px" });
