2a808b434f46827046298c2b775764852dca36f3
Jason Nutt <jnutt367@gmail.com>
2021-04-12 15:29:29 -0400

    commit newest with gsap making blk bgs for h2s

--- D3.js
+++ D3.js
@@ -9,6 +9,3 @@ d3.select("body").style("background-color", "white");
 d3.selectAll("h1").style("color", function() {
     return "hsl(" + Math.random() * 360 + ",100%,50%)";
 });
-d3.selectAll(".h2")
-    .data([8, 16, 32, 64, 128, 256])
-    .style("font-size", function(d) { return d + "px"; });
--- gsap.js
+++ gsap.js
@@ -1,3 +1,4 @@
 gsap.fromTo(".circle", { x: 100 }, { duration: "4", padding: "100", x: 100, y: -200, ease: "bounce.in" });
 
 gsap.fromTo(".rect", { x: 100 }, { duration: "2.5", y: "-150", rotate: "45" });
+gsap.to(".h2", { backgroundColor: "black", padding: "20" });
