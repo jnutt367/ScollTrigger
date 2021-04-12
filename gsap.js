33399ae78e2a6825fd05c1cffe3c6d6a5ac19b7b
Jason Nutt <jnutt367@gmail.com>
2021-04-12 16:54:09 -0400

    commit for baseball animation

--- gsap.js
+++ gsap.js
@@ -1,4 +1,12 @@
-gsap.fromTo(".circle", { x: 100 }, { duration: "4", padding: "100", x: 100, y: -200, ease: "bounce.in" });
+gsap.fromTo(".circle", { x: 50, y: -100 }, {
+    duration: "4",
+    padding: "100",
+    x: 100,
+    y: -900,
+    ease: "bounce.in",
 
-gsap.fromTo(".rect", { x: 100 }, { duration: "2.5", y: "-150", rotate: "45" });
+    border: "5px solid yellow"
+});
+
+gsap.fromTo(".rect", { x: 100 }, { duration: "2.5", y: "-150", rotate: "35" });
 gsap.to(".h2", { backgroundColor: "black", padding: "20" });
