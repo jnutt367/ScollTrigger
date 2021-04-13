b1130f9b803e359f397210204771e9f77ec019e6
Jason Nutt <jnutt367@gmail.com>
2021-04-12 22:00:32 -0400

    commit with correct spelling of gsap.js

--- gsap.js
+++ gsap.js
@@ -10,4 +10,7 @@ gsap.fromTo(".circle", { x: 50, y: -100 }, {
 
 gsap.fromTo(".rect", { x: 100 }, { duration: "2.5", y: "-150", rotate: "35" });
 gsap.to(".h2", { backgroundColor: "black", padding: "20" });
-gsap.to("h1", { padding: "20", borderRadius: "15px" });
+gsap.to("h1", { duration: 3, padding: "20", borderRadius: "15px", repeat: "-1" });
+
+Draggable.create("#jason");
+Draggable.create('svg');
